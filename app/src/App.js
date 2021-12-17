4985a0e16b9d9e95a0fd0df26c031b9c500c26fe
Shamil <toshamilgis@gmail.com>
2021-12-17 23:09:18 +0300

    firebase

--- app/src/App.js
+++ app/src/App.js
@@ -4,9 +4,25 @@ import { HomePage } from './routes/HomePage';
 import { LoginPage } from './routes/LoginPage';
 import { ProfilePage } from './routes/ProfilePage';
 import { SignUp } from './routes/RegistrationPage';
+import { PrivateRoute } from "./hocks/PrivateRoute";
+import { useEffect, useState } from 'react';
+import { auth } from './firebase/index';
 import './appStyles.css';
 
 export const App = () => {
+    const [authed, setAuthed] = useState(false);
+
+
+    useEffect(() => {
+        auth.onAuthStateChanged((user) => {
+            if (user) {
+                setAuthed(true);
+            } else {
+                setAuthed(false);
+            }
+        })
+    }, []);
+
   return (
     <div className="app">
         <Header/>
@@ -14,7 +30,11 @@ export const App = () => {
             <Routes>
                 <Route path="/login" element={<LoginPage />}/>
                 <Route path="/signup" element={<SignUp />}/>
-                <Route path="/profile" element={<ProfilePage />}/>
+                <Route path="/profile" element={
+                    <PrivateRoute authed={authed}>
+                        <ProfilePage />
+                    </PrivateRoute>
+                }/>
                 <Route path="/*" element={<HomePage />}/>
             </Routes>
         </div>
